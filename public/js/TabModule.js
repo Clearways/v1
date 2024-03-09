6f9b97e70d7eafb3c5cb72d451d163147ef146c4
rxfe-a <28RafaelA539@gmail.com>
2024-03-08 23:44:50 -0800

    tabamanabger

--- public/js/TabModule.js
+++ public/js/TabModule.js
@@ -3,3 +3,9 @@ let TabCounter = 0
 function NewTab() {
     TabCounter + 1
 }
+
+
+document.getElementById('TabHolder').onclick = function () {
+    var TBMGR = document.getElementById('tabmanagerc');
+    TBMGR.style.display = TBMGR.style.display === 'flex' ? 'none' : 'flex';
+}
